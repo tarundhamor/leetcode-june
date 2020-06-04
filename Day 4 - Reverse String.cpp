ae524739d42e10f8fe73a3f4d9f4a36bceba1e76
tarundhamor <tarundhamor@gmail.com>
2020-06-04 12:38:48 +0530

    Day 4

---
+++ Day 4 - Reverse String.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        return reverse(s.begin(),s.end());
+    }
+};
