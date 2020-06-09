0a1fa6177d5c6da22b5dbdf3c09a032a515fb450
tarundhamor <tarundhamor@gmail.com>
2020-06-09 22:28:46 +0530

    Day 9

---
+++ Day 9 - Is Subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int x =0;
+        for (int i =0; i<t.size() && x < s.size(); i++){
+            if(s[x] == t[i]){
+                x++;
+            }
+        }
+        return (x == s.size());
+        
+    }
+};
