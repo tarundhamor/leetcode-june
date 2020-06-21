5fd7a43c1f33dfbcdd159e7bc107a68ed074ad83
tarundhamor <tarundhamor@gmail.com>
2020-06-21 15:45:37 +0530

    Day 21

---
+++ Day 21 - Dungeon Game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        vector<int> dp(dungeon.size()+1,INT_MAX);
+        dp[dungeon.size()-1] = 1;
+        for(int i=dungeon[0].size()-1;i>=0;--i ){
+            for(int j = dungeon.size()-1;j>=0;--j){
+                dp[j] = max(1, min(dp[j+1],dp[j])-dungeon[j][i]);
+            }
+        }
+            return dp[0];
+    }
+};
