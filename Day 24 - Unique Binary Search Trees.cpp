34f72d17455f0de60a449ba87eb6528a39017a8e
tarundhamor <tarundhamor@gmail.com>
2020-06-24 15:46:41 +0530

    Day 24

---
+++ Day 24 - Unique Binary Search Trees.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numTrees(int n) {
+          long long ans =1;
+    for(int i=n+1;i<=2*n;i++){
+        ans = ans*i/(i-n);
+    }
+    return ans/(n+1);
+    }
+};
